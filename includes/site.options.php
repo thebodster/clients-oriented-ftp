a14851b13ed4e106708d920e4e7206bf994af642
powerdrummer <powerdrummer@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2010-05-23 03:58:51 +0000

    1) Javascript validation is done much better now. Uses functions (making it more versatile and not repeating the code on every ocasion). 2) Min and max length values for username and passwords are set via constants, on site.options.php. 3) better js variables names. There are still a few to correct. 4) JS validation changes the color of incorrect fields until validating correctly.

--- includes/site.options.php
+++ includes/site.options.php
@@ -33,4 +33,9 @@ $timeformat = $options_values['timeformat'];
 $database->Close();
 
 date_default_timezone_set($timezone);
+
+define('MIN_USER_CHARS', 6);
+define('MAX_USER_CHARS', 16);
+define('MIN_PASS_CHARS', 6);
+define('MAX_PASS_CHARS', 16);
 ?>
