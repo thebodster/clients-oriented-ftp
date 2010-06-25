f440fceef57491cc619de7783a7497341eb106dd
powerdrummer <powerdrummer@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2010-06-25 21:23:10 +0000

    New client and new user notifications are now using mail templates.

---
+++ emails/newclient.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>%SUBJECT%</title>
+</head>
+
+<body style="background:#f4f4f4;" bgcolor="#f4f4f4">
+<table width="550" border="0" cellspacing="0" cellpadding="0" style="margin:40px auto; background:#fff;	border:1px solid #ccc; -moz-border-radius:5px; -moz-box-shadow:3px 3px 5px #dedede; -webkit-border-radius:5px; -webkit-box-shadow:3px 3px 5px #dedede; border-radius:5px; box-shadow:3px 3px 5px #dedede;" bgcolor="#FFFFFF" align="center">
+	<tr>
+		<td style="padding:20px; font-family:Arial, Helvetica, sans-serif; font-size:12px;">
+			<h3 style="font-family:Georgia, 'Times New Roman', Times, serif; font-size:19px; font-weight:normal; padding-bottom:6px; border-bottom:1px dotted #CCCCCC; margin-bottom:20px; margin-top:0; color:#333333;">
+				%SUBJECT%
+			</h3>
+			<p>%BODY1%</p>
+			<p><strong>%LBLUSER%</strong>: %USERNAME%<br />
+			<strong>%LBLPASS%</strong>: %PASSWORD%</p>
+			<p>%BODY2%: %URI%</p>
+			<p>%BODY3%</p>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
