6f921bc3ba7437f0cb6a483ceb7916fc86a8a406
powerdrummer <powerdrummer@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2012-02-23 07:22:59 +0000

    Separate the database information from the other system constants/variables. This prevents overwriting that data when updating the software.

---
+++ includes/sys.config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// Enter your database info here
+define('DB_NAME', 'cftp_database'); // database name
+define('DB_HOST', 'localhost'); // database host (usually localhost)
+define('DB_USER', 'username'); // user related to cftp's database
+define('DB_PASSWORD', 'password'); // the password for that user
+?>
