8bfba7fbd32eeca736e08210022ce4299e7c8e9e
powerdrummer <powerdrummer@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2012-04-21 07:41:07 +0000

    This file was already loaded.

--- trunk/templates/session_check.php
+++ trunk/templates/session_check.php
@@ -17,17 +17,13 @@ $client_username = $this_user;
  * when you log in correctly.
  */
 if (isset($_SESSION['access']) && $_SESSION['access'] == 'admin') { $grant_access = 1; }
-if (isset($_SESSION['access']) && $_SESSION['access'] == $this_user) { $grant_access = 1; }
+if (isset($_SESSION['access']) && $_SESSION['access'] == $client_username) { $grant_access = 1; }
 if (isset($_COOKIE['access']) && $_COOKIE['access'] == 'admin') { $grant_access = 1; }
 if (isset($_COOKIE['access']) && $_COOKIE['access'] == $client_username) { $grant_access = 1; }
 
 /** If the info is not found, redirect to the log in page. */
 if (!isset($grant_access)) {
-	header("location:../../index.php");
+	header("location:".BASE_URI);
 	exit;
 }
-
-/** Continue loading the basic system files. */
-require_once('../../sys.vars.php');
-
 ?>
