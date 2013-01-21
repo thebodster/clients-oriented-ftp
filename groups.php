24e86d238d6eebf75e9f46b85443f87af48a9dde
info <info@subwaydesign.com.ar>
2013-01-21 09:00:13 +0000

    Manage files for groups.

--- groups.php
+++ groups.php
@@ -234,6 +234,7 @@ include('header.php');
 					<td><?php echo html_entity_decode($row["created_by"]); ?></td>
 					<td><?php echo date(TIMEFORMAT_USE,$row['timestamp']); ?></td>
 					<td>
+						<a href="manage-files.php?group_id=<?php echo $row["id"]; ?>" class="button button_blue"><?php _e('Manage files','cftp_admin'); ?></a>
 						<a href="groups-edit.php?id=<?php echo $row["id"]; ?>" class="button button_small button_blue"><?php _e('Edit','cftp_admin'); ?></a>
 					</td>
 				</tr>
