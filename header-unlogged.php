9d9d11047a53573ec627a78cecc4a39636a7cf52
info <info@subwaydesign.com.ar>
2013-01-21 03:15:24 +0000

    Lose focus after clicking a button (jquery).

--- header-unlogged.php
+++ header-unlogged.php
@@ -46,6 +46,14 @@ $database->MySQLDB();
 	
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.button').click(function() {
+				$(this).blur();
+			});
+		});
+	</script>
 </head>
 
 <body>
