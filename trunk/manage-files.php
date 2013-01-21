636573c3992106a0f80f30201416de69a834f704
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-01-21 06:57:09 +0000

    Manage files for a client. Still need to add a list of groups the client belongs to, and files on those groups.

--- trunk/manage-files.php
+++ trunk/manage-files.php
@@ -13,7 +13,7 @@ require_once('sys.includes.php');
  * The client's id is passed on the URI.
  * Then get_client_by_id() gets all the other account values.
  */
-$this_id = $_GET['id'];
+$this_id = $_GET['client_id'];
 $this_client = get_client_by_id($this_id);
 
 $page_title = __('Manage files','cftp_admin');
@@ -52,7 +52,16 @@ include('header.php');
 				var action = $('#files_actions').val();
 				if (action == 'delete') {
 					var msg_1 = '<?php _e("You are about to delete",'cftp_admin'); ?>';
-					var msg_2 = '<?php _e("files. Are you sure you want to continue?",'cftp_admin'); ?>';
+					var msg_2 = '<?php _e("files permanently and for every client/group. Are you sure you want to continue?",'cftp_admin'); ?>';
+					if (confirm(msg_1+' '+checks.length+' '+msg_2)) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+				else if (action == 'unassign') {
+					var msg_1 = '<?php _e("You are about to unassign",'cftp_admin'); ?>';
+					var msg_2 = '<?php _e("files from this account. Are you sure you want to continue?",'cftp_admin'); ?>';
 					if (confirm(msg_1+' '+checks.length+' '+msg_2)) {
 						return true;
 					} else {
@@ -63,11 +72,6 @@ include('header.php');
 		});
 
 	});
-
-	function confirm_file_delete() {
-		if (confirm("<?php _e('This will delete the file permanently. Continue?','cftp_template'); ?>")) return true ;
-		else return false ;
-	}
 </script>
 
 <div id="main">
@@ -125,6 +129,18 @@ include('header.php');
 							echo system_message('ok',$msg);
 							break;
 
+						case 'unassign':
+							/**
+							 * Remove the file from this client's account only.
+							 */
+							foreach ($selected_files as $work_file) {
+								$this_file = new FilesActions();
+								$unassign_file = $this_file->unassign_file($work_file);
+							}
+							$msg = __('The selected files were unassigned from this client.','cftp_admin');
+							echo system_message('ok',$msg);
+							break;
+
 						case 'delete':
 							foreach ($selected_files as $work_file) {
 								$this_file = new FilesActions();
@@ -142,15 +158,8 @@ include('header.php');
 			}
 
 			$database->MySQLDB();
-			$cq = 'SELECT * FROM tbl_files WHERE client_user="' . $this_client['username'] .'"';
+			$cq = 'SELECT * FROM tbl_files_relations WHERE client_id="' . $this_id .'"';
 
-			/** Add the search terms */	
-			if(isset($_POST['search']) && !empty($_POST['search'])) {
-				$search_terms = $_POST['search'];
-				$cq .= " AND (filename LIKE '%$search_terms%' OR description LIKE '%$search_terms%')";
-				$no_results_error = 'search';
-			}
-		
 			/** Add the status filter */	
 			if(isset($_POST['status']) && $_POST['status'] != 'all') {
 				$status_filter = $_POST['status'];
@@ -177,16 +186,44 @@ include('header.php');
 			 * an error message.
 			 */
 			$sql = $database->query($cq);
-			$count = mysql_num_rows($sql);
+
+			if (mysql_num_rows($sql) > 0) {
+				/**
+				 * Get the IDs of files that match the previous query.
+				 */
+				while($row_files = mysql_fetch_array($sql)) {
+					$files_ids[] = $row_files['file_id'];
+					$gotten_files = implode(',',$files_ids);
+				}
+				
+				/**
+				 * Get the files
+				 */
+				$fq = "SELECT * from tbl_files WHERE id IN ($gotten_files)";
+	
+				/** Add the search terms */	
+				if(isset($_POST['search']) && !empty($_POST['search'])) {
+					$search_terms = $_POST['search'];
+					$fq .= " AND (filename LIKE '%$search_terms%' OR description LIKE '%$search_terms%')";
+					$no_results_error = 'search';
+				}
+	
+				$sql_files = $database->query($fq);
+				$count = mysql_num_rows($sql_files);
+			}
+			else {
+				$count = 0;
+				$no_results_error = 'filter';
+			}
 		?>
 			<div class="form_actions_left">
 				<div class="form_actions_limit_results">
-					<form action="manage-files.php?id=<?php echo $this_id; ?>" name="files_search" method="post" class="inline_form">
+					<form action="manage-files.php?client_id=<?php echo $this_id; ?>" name="files_search" method="post" class="inline_form">
 						<input type="text" name="search" id="search" value="<?php if(isset($_POST['search']) && !empty($_POST['search'])) { echo $_POST['search']; } ?>" class="txtfield form_actions_search_box" />
 						<input type="submit" id="btn_proceed_search" value="<?php _e('Search','cftp_admin'); ?>" class="button_form" />
 					</form>
 	
-					<form action="manage-files.php?id=<?php echo $this_id; ?>" name="files_filters" method="post" class="inline_form">
+					<form action="manage-files.php?client_id=<?php echo $this_id; ?>" name="files_filters" method="post" class="inline_form">
 						<select name="status" id="status" class="txtfield">
 							<option value="all"><?php _e('All statuses','cftp_admin'); ?></option>
 							<option value="1"><?php _e('Hidden','cftp_admin'); ?></option>
@@ -203,7 +240,7 @@ include('header.php');
 				</div>
 			</div>
 	
-			<form action="manage-files.php?id=<?php echo $this_id; ?>" name="files_list" method="post">
+			<form action="manage-files.php?client_id=<?php echo $this_id; ?>" name="files_list" method="post">
 				<div class="form_actions_right">
 					<div class="form_actions">
 						<div class="form_actions_submit">
@@ -211,7 +248,8 @@ include('header.php');
 							<select name="files_actions" id="files_actions" class="txtfield">
 								<option value="hide"><?php _e('Hide','cftp_admin'); ?></option>
 								<option value="show"><?php _e('Show','cftp_admin'); ?></option>
-								<option value="delete"><?php _e('Delete','cftp_admin'); ?></option>
+								<option value="unassign"><?php _e('Unassign','cftp_admin'); ?></option>
+								<option value="delete"><?php _e('Delete for everyone','cftp_admin'); ?></option>
 							</select>
 							<input type="submit" name="do_action" id="do_action" value="<?php _e('Proceed','cftp_admin'); ?>" class="button_form" />
 						</div>
@@ -235,6 +273,9 @@ include('header.php');
 									break;
 								case 'filter':
 									$no_results_message = __('The filters you selected returned no results.','cftp_admin');;
+									break;
+								case 'none_assigned':
+									$no_results_message = __('There are no files assigned to this client.','cftp_admin');;
 									break;
 							}
 						}
@@ -264,30 +305,39 @@ include('header.php');
 					<tbody>
 						<?php
 							if ($count > 0) {
-								while($row = mysql_fetch_array($sql)) {
+								while($row = mysql_fetch_array($sql_files)) {
 									/**
 									 * Construct the complete file URI to use on the download button.
 									 */
 									$this_file_uri = UPLOADED_FILES_FOLDER.$row['url'];
+									$sql_this_file = $database->query("SELECT * FROM tbl_files_relations WHERE file_id='".$row['id']."' AND client_id = ".$this_id);
+									while($data_file = mysql_fetch_array($sql_this_file)) {
+										$file_id = $data_file['id'];
+										$hidden = $data_file['hidden'];
+										$download_count = $data_file['download_count'];
+									}
 						?>
 									<tr>
-										<td><input type="checkbox" name="files[]" value="<?php echo $row["id"]; ?>" /></td>
+										<td><input type="checkbox" name="files[]" value="<?php echo $file_id; ?>" /></td>
 										<td><?php echo date(TIMEFORMAT_USE, $row['timestamp']); ?></td>
 										<td><strong><?php echo htmlentities($row['filename']); ?></strong></td>
 										<td><?php echo htmlentities($row['description']); ?></td>
 										<td><?php $this_file = filesize($this_file_uri); echo format_file_size($this_file); ?></td>
-										<td class="<?php echo ($row['hidden'] === '1') ? 'file_status_hidden' : 'file_status_visible'; ?>">
+										<td class="<?php echo ($hidden === '1') ? 'file_status_hidden' : 'file_status_visible'; ?>">
 											<?php
 												$status_hidden = __('Hidden','cftp_admin');
 												$status_visible = __('Visible','cftp_admin');
-												echo ($row['hidden'] === '1') ? $status_hidden : $status_visible;
+												echo ($hidden === '1') ? $status_hidden : $status_visible;
 											?>
 										</td>
 										<td><?php echo $row['uploader']; ?></td>
-										<td><?php echo $row['download_count']; ?></td>
+										<td><?php echo $download_count; ?></td>
 										<td>
 											<a href="<?php echo $this_file_uri; ?>" target="_blank" class="button button_blue">
 												<?php _e('Download','cftp_template'); ?>
+											</a>
+											<a href="edit-file.php?file_id=<?php echo $row["id"]; ?>" target="_blank" class="button button_blue">
+												<?php _e('Edit file','cftp_template'); ?>
 											</a>
 										</td>
 									</tr>
