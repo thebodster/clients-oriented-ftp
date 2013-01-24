05b0da76fc6ff94a006bf439ee282d08634f31e3
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-01-24 00:14:44 +0000

    Gallery template fixed.

--- trunk/templates/gallery/template.php
+++ trunk/templates/gallery/template.php
@@ -12,16 +12,28 @@ include_once(ROOT_DIR.'/templates/common.php'); // include the required function
 
 $window_title = __('Gallery','cftp_template_gallery');
 
+/**
+ * Filter files by type, only save images.
+*/
+$img_formats = array('gif','jpg','pjpeg','jpeg','png');
+foreach ($my_files as $file) {
+	$pathinfo = pathinfo($file['url']);
+	$extension = strtolower($pathinfo['extension']);
+	if (in_array($extension,$img_formats)) {
+		$img_files[] = $file;
+	}
+}
+$count = count($img_files);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php echo $user_full_name.' | '.$window_title; ?> | <?php echo SYSTEM_NAME; ?></title>
-<link rel="stylesheet" media="all" type="text/css" href="<?php echo $this_template; ?>main.css" />
-<link rel="shortcut icon" href="<?php echo BASE_URI; ?>/favicon.ico" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
-<link href='http://fonts.googleapis.com/css?family=Sirin+Stencil' rel='stylesheet' type='text/css'>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php echo $client_info['name'].' | '.$window_title; ?> | <?php echo SYSTEM_NAME; ?></title>
+	<link rel="stylesheet" media="all" type="text/css" href="<?php echo $this_template; ?>main.css" />
+	<link rel="shortcut icon" href="<?php echo BASE_URI; ?>/favicon.ico" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+	<link href='http://fonts.googleapis.com/css?family=Sirin+Stencil' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
@@ -42,40 +54,42 @@ $window_title = __('Gallery','cftp_template_gallery');
 	<div class="wrapper">
 
 <?php
-	$count = mysql_num_rows($template_files_sql);
 	if (!$count) {
 		_e('There are no files.','cftp_template_gallery');
 	}
 	else {
 ?>
 		<ul class="photo_list">
-		<?php
-			while($row = mysql_fetch_array($template_files_sql)) {
-		?>
 			<?php
-				$pathinfo = pathinfo($row['url']);
-				$extension = strtolower($pathinfo['extension']);
-				$img_formats = array('gif','jpg','pjpeg','jpeg','png');
-				if (in_array($extension,$img_formats)) {
+				foreach ($img_files as $file) {
+					$download_link = BASE_URI.
+										'process.php?do=download
+										&amp;client='.$this_user.'
+										&amp;client_id='.$client_info['id'].'
+										&amp;url='.$file['url'].'
+										&amp;id='.$file['id'].'
+										&amp;origin='.$file['origin'];
+					if (!empty($file['group_id'])) {
+						$download_link .= '&amp;group_id='.$file['group_id'];
+					}
 			?>
 					<li>
-						<h5><?php echo htmlentities($row['filename']); ?></h5>
+						<h5><?php echo htmlentities($file['name']); ?></h5>
 						<div class="img_prev">
 							<a href="<?php echo BASE_URI; ?>process.php?do=download&amp;client=<?php echo $this_user; ?>&amp;file=<?php echo $row['url']; ?>" target="_blank">
-								<img src="<?php echo $this_template; ?>/timthumb.php?src=<?php echo BASE_URI.UPLOADED_FILES_URL; echo $row['url']; ?>&amp;w=280&amp;h=215&amp;f=2" class="thumbnail" alt="" />
+								<img src="<?php echo $this_template; ?>/timthumb.php?src=<?php echo BASE_URI.UPLOADED_FILES_URL; echo $file['url']; ?>&amp;w=280&amp;h=215&amp;f=2" class="thumbnail" alt="" />
 							</a>
 						</div>
 						<div class="img_data">
 							<div class="download_link">
-								<a href="<?php echo BASE_URI; ?>process.php?do=download&amp;client=<?php echo $this_user; ?>&amp;file=<?php echo $row['url']; ?>" target="_blank">
+								<a href="<?php echo $download_link; ?>" target="_blank">
 									<?php _e('Download original','cftp_template_gallery'); ?>
 								</a>
 							</div>
 						</div>
 					</li>
-				<?php
+			<?php
 				}
-			}
 			?>
 		</ul>
 	<?php
