63010efc1bbb5eeb86df35adf24e3d289c41ab1f
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-01-25 21:02:50 +0000

    New dashboard, instead of the previous home page.

--- trunk/home.php
+++ trunk/home.php
@@ -17,195 +17,54 @@ $database->MySQLDB();
 <div id="main">
 	<h2><?php echo $page_title; ?></h2>
 
-	<?php
-	/**
-	 * Icons are from the set: Origami Colored Pencil
-	 *
-	 * @link		http://www.iconfinder.com/search/?q=iconset%3Acolorful
-	 * @author		Double-J designs
-	 */
-	?>
 	<div class="home">
 		<div class="container-fluid">
 			<div class="row-fluid">
-				<div class="span4">
-					<ul class="home_spaces">
-						<?php
-							/** Show SIMPLE UPLOAD widget and current logo to clients */
-							$upload_allowed = array(0);
-							if (in_session_or_cookies($upload_allowed)) {
-						?>
-								<li class="logo_home_li">
-									<div class="home_container">
-										<img src="<?php echo BASE_URI; ?>includes/timthumb/timthumb.php?src=<?php echo BASE_URI; ?>img/custom/logo/<?php echo LOGO_FILENAME; ?>&amp;w=300" alt="<?php echo SYSTEM_NAME; ?> Logo" />
+			<?php
+				$log_allowed = array(9);
+				if (in_session_or_cookies($log_allowed)) {
+					$show_log = true;
+				}
+			?>
+					<div class="span8 <?php if ($show_log != true) { echo 'offset2'; } ?>">
+						<div class="row-fluid">
+							<div class="span12">
+								<div class="widget">
+									<h4><?php _e('Statistics for the last 15 days','cftp_admin'); ?></h4>
+									<div class="widget_int">
+										<div id="statistics" style="height:320px;width:100%;"></div>
+										<ul class="graph_legend">
+											<li><div class="legend_color legend_color1"></div><?php _e('Uploads','cftp_admin'); ?></li><li>
+											<div class="legend_color legend_color2"></div><?php _e('Downloads','cftp_admin'); ?></li><li>
+											<div class="legend_color legend_color3"></div><?php _e('Zip Downloads','cftp_admin'); ?></li>
+										</ul>
 									</div>
-								</li>
-
-								<li class="home_widget_small">
-									<div class="home_container">
-										<h4><?php _e('Files','cftp_admin'); ?></h4>
-										<img src="img/home-widget-files.png" alt="" />
-										<a href="upload-from-computer.php" class="button button_blue button_big"><?php _e('Upload from computer','cftp_admin'); ?></a>
-										<a href="<?php echo BASE_URI.'my_files/'; ?>" class="button button_blue button_big"><?php _e('Access my files list','cftp_admin'); ?></a>
-										<div class="message message_info">
-											<p><?php _e('Total files on account:','cftp_admin'); ?>
-												<strong>
-												<?php
-													/** Count the VISIBLE files on this client's account */
-													$my_username = get_current_user_username();
-													$sql = $database->query("SELECT distinct id FROM tbl_files WHERE client_user = '$my_username' AND hidden = '0'");
-													$total_files = mysql_num_rows($sql);
-													echo $total_files;
-												?>
-												</strong>
-											</p>
-										</div>
+								</div>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span6">
+								<?php include(ROOT_DIR.'/home-news-widget.php'); ?>
+							</div>
+							<div class="span6">
+								<div class="widget">
+									<h4><?php _e('System information','cftp_admin'); ?></h4>
+									<div class="widget_int">
+										<div id="sys_info" style="height:300px;width:100%; "></div>
 									</div>
-								</li>
-						<?php
-							}
-							/** Show COMPLETE UPLOAD widget to allowed users */
-							$upload_allowed = array(9,8,7);
-							if (in_session_or_cookies($upload_allowed)) {
-						?>
-								<li class="home_widget_small">
-									<div class="home_container">
-										<h4><?php _e('Upload files','cftp_admin'); ?></h4>
-										<img src="img/home-widget-files.png" alt="" />
-										<a href="upload-from-computer.php" class="button button_blue button_big"><?php _e('Upload from computer','cftp_admin'); ?></a>
-										<a href="upload-by-ftp.php" class="button button_blue button_big"><?php _e('Find orphan files','cftp_admin'); ?></a>
-										<div class="message message_info">
-											<p><?php _e('Total files:','cftp_admin'); ?>
-												<strong>
-												<?php
-													$sql = $database->query("SELECT distinct id FROM tbl_files");
-													$total_files = mysql_num_rows($sql);
-													echo $total_files;
-												?>
-												</strong>
-											</p>
-										</div>
-									</div>
-								</li>
-						<?php
-							}
-							/** Show CLIENTS widget to allowed users */
-							$clients_allowed = array(9,8);
-							if (in_session_or_cookies($clients_allowed)) {
-						?>
-								<li class="home_widget_small">
-									<div class="home_container">
-										<?php
-											if ($count_inactive > 0) {
-										?>
-												<div class="home_inactive_msg">
-													<?php echo $count_inactive; ?>
-												</div>
-										<?php
-											}
-										?>
-										<h4><?php _e('Clients','cftp_admin'); ?></h4>
-										<img src="img/home-widget-clients.png" alt="" />
-										<a href="clients-add.php" class="button button_blue button_big"><?php _e('Add new client','cftp_admin'); ?></a>
-										<a href="clients.php" class="button button_blue button_big"><?php _e('Manage clients','cftp_admin'); ?></a>
-										<div class="message message_info">
-											<p><?php _e('Total clients:','cftp_admin'); ?>
-												<strong>
-												<?php
-													$sql = $database->query("SELECT distinct user FROM tbl_users WHERE level='0'");
-													$total_clients = mysql_num_rows($sql);
-													echo $total_clients;
-												?>
-												</strong>
-											</p>
-										</div>
-									</div>
-								</li>
-						<?php
-							}
-				
-							/** Show GROUPS widget to allowed users */
-							$groups_allowed = array(9,8);
-							if (in_session_or_cookies($groups_allowed)) {
-						?>
-								<li class="home_widget_small">
-									<div class="home_container">
-										<h4><?php _e('Clients groups','cftp_admin'); ?></h4>
-										<img src="img/home-widget-groups.png" alt="" />
-										<a href="groups-add.php" class="button button_blue button_big"><?php _e('Add new group','cftp_admin'); ?></a>
-										<a href="groups.php" class="button button_blue button_big"><?php _e('Manage groups','cftp_admin'); ?></a>
-										<div class="message message_info">
-											<p><?php _e('Total groups:','cftp_admin'); ?>
-												<strong>
-												<?php
-													$sql = $database->query("SELECT distinct id FROM tbl_groups");
-													$total_groups = mysql_num_rows($sql);
-													echo $total_groups;
-												?>
-												</strong>
-											</p>
-										</div>
-									</div>
-								</li>
-						<?php
-							}
-				
-							/** Show USERS and BRANDING widgets to system administrators (Level 9) only */
-							$users_allowed = array(9);
-							if (in_session_or_cookies($users_allowed)) {
-						?>
-								<li class="home_widget_small">
-									<div class="home_container">
-										<h4><?php _e('System users','cftp_admin'); ?></h4>
-										<img src="img/home-widget-users.png" alt="" />
-										<a href="users-add.php" class="button button_blue button_big"><?php _e('Add new user','cftp_admin'); ?></a>
-										<a href="users.php" class="button button_blue button_big"><?php _e('Manage users','cftp_admin'); ?></a>
-										<div class="message message_info">
-											<p><?php _e('Total users:','cftp_admin'); ?>
-												<strong>
-												<?php
-													$sql = $database->query("SELECT distinct user FROM tbl_users WHERE level != '0'");
-													$total_users = mysql_num_rows($sql);
-													echo $total_users;
-												?>
-												</strong>
-											</p>
-										</div>
-									</div>
-								</li>
-								<li class="home_widget_small">
-									<div class="home_container">
-										<h4><?php _e('Branding','cftp_admin'); ?></h4>
-										<img src="img/home-widget-branding.png" alt="" />
-										<a href="branding.php" class="button button_blue button_big"><?php _e('Change Logo','cftp_admin'); ?></a>
-										<p><?php _e("Upload your company logo. It will appear in every client's page.", 'cftp_admin'); ?></p>
-									</div>
-								</li>
-						<?php
-							}
-						?>
-					</ul>
-				</div>
-
-				<div class="span8">	
-					<div class="row-fluid">
-						<div class="span12">
-							<div class="widget">
-								<h4><?php _e('Statistics for the last 15 days','cftp_admin'); ?></h4>
-								<div class="widget_int">
-									<div id="statistics" style="height:300px;width:100%;"></div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="row-fluid">
-						<div class="span6">
+					
+					<?php if (isset($show_log) && $show_log == true) { ?>
+						<div class="span4">
 							<div class="widget">
 								<h4><?php _e('Recent activites','cftp_admin'); ?></h4>
 								<div class="widget_int">
 									<ul class="activities_log">
 										<?php
-											$sql_log = $database->query("SELECT * FROM tbl_actions_log ORDER BY id DESC LIMIT 6");
+											$sql_log = $database->query("SELECT * FROM tbl_actions_log ORDER BY id DESC LIMIT 14");
 											$log_count = mysql_num_rows($sql_log);
 											if ($log_count > 0) {
 												while($log = mysql_fetch_array($sql_log)) {
@@ -234,22 +93,29 @@ $database->MySQLDB();
 								</div>
 							</div>
 						</div>
-						<div class="span6">
-							<div class="widget">
-								<h4><?php _e('System information','cftp_admin'); ?></h4>
-								<div class="widget_int">
-									<div id="sys_info" style="height:340px;width:100%; "></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<?php
+					}
+				?>
 			</div>
 		</div>
 	</div>
 	
 </div>
 
+<?php
+	/** Get the data to show on the bars graphic */
+	$sql = $database->query("SELECT distinct id FROM tbl_files");
+	$total_files = mysql_num_rows($sql);
+
+	$sql = $database->query("SELECT distinct user FROM tbl_users WHERE level='0'");
+	$total_clients = mysql_num_rows($sql);
+
+	$sql = $database->query("SELECT distinct id FROM tbl_groups");
+	$total_groups = mysql_num_rows($sql);
+
+	$sql = $database->query("SELECT distinct user FROM tbl_users WHERE level != '0'");
+	$total_users = mysql_num_rows($sql);
+?>
 <script type="text/javascript">
 	$(document).ready(function(){
 		$.plot(
@@ -296,7 +162,7 @@ $database->MySQLDB();
 		);
 
 		// statistics
-		<?php include('home_statistics.php'); ?>
+		<?php include(ROOT_DIR.'/home-statistics.php'); ?>
 
 		function showTooltip(x, y, contents) {
 			$('<div id="stats_tooltip">' + contents + '</div>').css( {
