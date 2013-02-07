9ce7cfd0e65c87f528eab43a3ea30b755493ff44
info <info@subwaydesign.com.ar>
2013-02-07 06:35:10 +0000

    Release

--- sys.vars.php
+++ sys.vars.php
@@ -14,7 +14,7 @@
  * Current version.
  * Updated only when releasing a new downloadable complete version.
  */
-define('CURRENT_VERSION', 'r325');
+define('CURRENT_VERSION', 'r330');
 
 /**
  * Turn off reporting of PHP errors, warnings and notices.
