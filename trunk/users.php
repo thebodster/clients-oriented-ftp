5c762dd226ffad8560f31ab323a5bf1f28b511e2
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-02-18 06:59:37 +0000

    Fixed a bug preventing the log from working when selecting multiple users/clients.

--- trunk/users.php
+++ trunk/users.php
@@ -69,7 +69,7 @@ include('header.php');
 			/**
 			 * Make a list of users to avoid individual queries.
 			 */
-			$sql_user = $database->query("SELECT id, name FROM tbl_users WHERE id IN ('$users_to_get')");
+			$sql_user = $database->query("SELECT id, name FROM tbl_users WHERE id IN ($users_to_get)");
 			while($data_user = mysql_fetch_array($sql_user)) {
 				$all_users[$data_user['id']] = $data_user['name'];
 			}
