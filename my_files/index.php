dfeccf073d38d377ba1864525291062ce746179c
info <info@subwaydesign.com.ar>
2013-02-20 22:49:36 +0000

    Fix the previous commit.

--- my_files/index.php
+++ my_files/index.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../sys.includes.php');
-	$this_user = get_current_user_username();
+	$this_user = CURRENT_USER_USERNAME;
 	if (!empty($_GET['client'])) {
 		$this_user = $_GET['client'];
 	}
