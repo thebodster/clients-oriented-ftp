efce17b0ed3c7610ce50d72adc2ffffb795394b1
info <info@subwaydesign.com.ar>
2013-02-21 03:12:45 +0000

    Language files (spanish version by Raúl Elenes) and prep for release.

--- sys.vars.php
+++ sys.vars.php
@@ -14,7 +14,7 @@
  * Current version.
  * Updated only when releasing a new downloadable complete version.
  */
-define('CURRENT_VERSION', 'r365');
+define('CURRENT_VERSION', 'r370');
 
 /**
  * Turn off reporting of PHP errors, warnings and notices.
