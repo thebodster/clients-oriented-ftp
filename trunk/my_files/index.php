4181393faae5ff84da5f7bb66eed5d69260a58a4
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-05-06 22:39:36 +0000

    Prevent viewing others private files

--- trunk/my_files/index.php
+++ trunk/my_files/index.php
@@ -1,7 +1,7 @@
 <?php
 	require_once('../sys.includes.php');
 	$this_user = CURRENT_USER_USERNAME;
-	if (!empty($_GET['client'])) {
+	if (!empty($_GET['client']) && CURRENT_USER_LEVEL != '0') {
 		$this_user = $_GET['client'];
 	}
 	include_once(TEMPLATE_PATH);
