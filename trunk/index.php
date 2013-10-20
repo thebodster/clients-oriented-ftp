861340b2eb518f9b366a4f922e2a9ce41e98612b
info@subwaydesign.com.ar <info@subwaydesign.com.ar@c33e8c38-1a77-9f26-60fa-bed1ac63749c>
2013-10-20 07:33:19 +0000

    hide the version number on the footer for unlogged users. Also, some HTML and CSS changes to the login form, and the new password recovery (empty for now) page.

--- trunk/index.php
+++ trunk/index.php
@@ -95,84 +95,91 @@ include('header-unlogged.php');
 	
 	}
 	?>
-		
-		<div class="whiteform whitebox" id="loginform">
-			<?php
-				/**
-				 * Show login errors
-				 */
-				if (isset($errorstate)) {
-					switch ($errorstate) {
-						case 'wrong_username':
-							$login_err_message = __("The supplied username doesn't exist.",'cftp_admin');
-							break;
-						case 'wrong_password':
-							$login_err_message = __("The supplied password is incorrect.",'cftp_admin');
-							break;
-						case 'inactive_client':
-							$login_err_message = __("This account is not active. If you just registered, please wait until a system administrator approves your account.",'cftp_admin');
-							break;
-					}
-	
-					echo system_message('error',$login_err_message,'login_error');
-				}
-			?>
-		
-			<script type="text/javascript">
-				$(document).ready(function() {
-					$("form").submit(function() {
-						clean_form(this);
-		
-						is_complete(this.login_form_user,'<?php _e('Username was not completed','cftp_admin'); ?>');
-						is_complete(this.login_form_pass,'<?php _e('Password was not completed','cftp_admin'); ?>');
-		
-						// show the errors or continue if everything is ok
-						if (show_form_errors() == false) { return false; }
-					});
-				});
-			</script>
-		
-			<form action="index.php" method="post" name="login_admin">
-				<input type="hidden" name="sent_admin" id="sent_admin">
-				<ul class="form_fields">
-					<li>
-						<label for="login_form_user"><?php _e('Username','cftp_admin'); ?></label>
-						<input type="text" name="login_form_user" id="login_form_user" value="<?php if (isset($sysuser_username)) { echo $sysuser_username; } ?>" class="field" />
-					</li>
-					<li>
-						<label for="login_form_pass"><?php _e('Password','cftp_admin'); ?></label>
-						<input type="password" name="login_form_pass" id="login_form_pass" class="field" />
-					</li>
-					<li>
-						<label for="login_form_remember"><?php _e('Remember me','cftp_admin'); ?></label>
-						<input type="checkbox" name="login_form_remember" id="login_form_remember" value="on" />
-					</li>
-					<li class="form_submit_li">
-						<input type="submit" name="Submit" value="<?php _e('Continue to log in','cftp_admin'); ?>" id="button_login" class="button button_blue button_submit" />
-					</li>
-				</ul>
-			</form>
 
-			<div class="login_form_links">
-				<?php
-					if (CLIENTS_CAN_REGISTER == '1') {
-				?>
-						<p><?php _e("Don't have an account yet?",'cftp_admin'); ?> <a href="<?php echo BASE_URI; ?>register.php"><?php _e('Register as a new client.','cftp_admin'); ?></a></p>
-				<?php
-					} else {
-				?>
-						<p><?php _e("This server does not allow self registrations.",'cftp_admin'); ?></p>
-						<p><?php _e("If you need an account, please contact a server administrator.",'cftp_admin'); ?></p>
-				<?php
-					}
-				?>
-			</div>
-	
-		</div>
-	
-	</div> <!-- main -->
+		<h2><?php echo $page_title; ?></h2>
+		
+		<div class="container">
+			<div class="row">
+				<div class="span4 offset4 white-box">
+					<div class="white-box-interior">
+						<?php
+							/**
+							 * Show login errors
+							 */
+							if (isset($errorstate)) {
+								switch ($errorstate) {
+									case 'wrong_username':
+										$login_err_message = __("The supplied username doesn't exist.",'cftp_admin');
+										break;
+									case 'wrong_password':
+										$login_err_message = __("The supplied password is incorrect.",'cftp_admin');
+										break;
+									case 'inactive_client':
+										$login_err_message = __("This account is not active. If you just registered, please wait until a system administrator approves your account.",'cftp_admin');
+										break;
+								}
+				
+								echo system_message('error',$login_err_message,'login_error');
+							}
+						?>
+					
+						<script type="text/javascript">
+							$(document).ready(function() {
+								$("form").submit(function() {
+									clean_form(this);
+					
+									is_complete(this.login_form_user,'<?php _e('Username was not completed','cftp_admin'); ?>');
+									is_complete(this.login_form_pass,'<?php _e('Password was not completed','cftp_admin'); ?>');
+					
+									// show the errors or continue if everything is ok
+									if (show_form_errors() == false) { return false; }
+								});
+							});
+						</script>
+					
+						<form action="index.php" method="post" name="login_admin" role="form">
+							<fieldset>
+								<input type="hidden" name="sent_admin" id="sent_admin">
 
-	<?php default_footer_info(); ?>
+								<label for="login_form_user"><?php _e('Username','cftp_admin'); ?></label>
+								<input type="text" name="login_form_user" id="login_form_user" value="<?php if (isset($sysuser_username)) { echo $sysuser_username; } ?>" class="field" />
+
+								<label for="login_form_pass"><?php _e('Password','cftp_admin'); ?></label>
+								<input type="password" name="login_form_pass" id="login_form_pass" class="field" />
+
+								<label for="login_form_remember">
+									<input type="checkbox" name="login_form_remember" id="login_form_remember" value="on" />
+									<?php _e('Remember me','cftp_admin'); ?>
+								</label>
+
+								<div class="form_submit_li">
+									<button type="submit" name="submit" id="button_login" class="button button_blue button_submit"><?php _e('Continue','cftp_admin'); ?></button>
+								</div>
+							</fieldset>
+						</form>
+			
+						<div class="login_form_links">
+							<p id="reset_pass_link"><?php _e("Forgot your password?",'cftp_admin'); ?> <a href="<?php echo BASE_URI; ?>reset-password.php"><?php _e('Set up a new one.','cftp_admin'); ?></a></p>
+							<?php
+								if (CLIENTS_CAN_REGISTER == '1') {
+							?>
+									<p id="register_link"><?php _e("Don't have an account yet?",'cftp_admin'); ?> <a href="<?php echo BASE_URI; ?>register.php"><?php _e('Register as a new client.','cftp_admin'); ?></a></p>
+							<?php
+								} else {
+							?>
+									<p><?php _e("This server does not allow self registrations.",'cftp_admin'); ?></p>
+									<p><?php _e("If you need an account, please contact a server administrator.",'cftp_admin'); ?></p>
+							<?php
+								}
+							?>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div> <!-- container -->
+	</div> <!-- main (from header) -->
+
+	<?php default_footer_info(false); ?>
 
 </body>
 </html>
